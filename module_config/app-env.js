a87231fd97ea8da66bd62ea46efb3c9c6d8df714
mschimk1 <mschimko@gmail.com>
2016-07-29 12:57:38 +1000

    Change app env module export

--- module_config/app-env.js
+++ module_config/app-env.js
@@ -1,17 +1,11 @@
 'use strict';
 
-class Environment {
-    constructor() {
-        const env = require('cfenv').getAppEnv(), LOCAL_ENV_PORT = 3000;
-        this.app_name = env.app_name;
-        this.environment = process.env.NODE_ENV;
-        this.port = env.isLocal ? LOCAL_ENV_PORT : env.port;
-        this.url = env.URL || "http://localhost";
-        this.secure = process.env.VCAP_APPLICATION ? true : false;
-    }
-    toString() {
-        return "TODO";   
-    }
-}
+const env = require('cfenv').getAppEnv(), LOCAL_ENV_PORT = 3000;
 
-module.exports = Environment;
+module.exports = {
+  app_name : env.name,
+  environment : process.env.NODE_ENV,
+  port : env.isLocal ? LOCAL_ENV_PORT : env.port,
+  url : env.URL || "http://localhost",
+  secure : process.env.VCAP_APPLICATION ? true : false
+};
--- server.js
+++ server.js
@@ -4,15 +4,14 @@
 require('dotenv').config({silent: true});
 
 const logger  = require('winston');
-const Environment = require('./module_config/app-env');
-let env = new Environment();
+const appEnv = require('./module_config/app-env');
 
 // Deployment tracking
 require('cf-deployment-tracker-client').track();
 
 const server = require('./app');
-const port = process.env.PORT || process.env.VCAP_APP_PORT || env.port;
+const port = process.env.PORT || process.env.VCAP_APP_PORT || appEnv.port;
 
 server.listen(port, () => {
-  logger.info('Server listening on %s:%s', env.url, env.port);
+  logger.info(`Server listening on ${appEnv.url}:${appEnv.port}`);
 });
